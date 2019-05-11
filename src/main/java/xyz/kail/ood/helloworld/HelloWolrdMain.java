b6daefc058c6d19a8753d25029aec699906cc43d
杨凯彬 <ykb553@163.com>
2019-05-11 22:41:48 +0800

    System.out.

--- src/main/java/xyz/kail/ood/helloworld/HelloWolrdMain.java
+++ src/main/java/xyz/kail/ood/helloworld/HelloWolrdMain.java
@@ -3,6 +3,7 @@ package xyz.kail.ood.helloworld;
 public class HelloWolrdMain {
 
     public static void main(String[] args) {
+        // System.out.
         System.out.println("Hello Wolrd");
     }
 
